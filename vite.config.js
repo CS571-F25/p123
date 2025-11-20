6bca6372b3bc79b3d31b05067874ca1f73f571e5
ira <ihande@wisc.edu>
2025-11-19 23:36:33 -0600

    live website

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-plugins: [react()],
-base: '/p0/',
-build: {
-outDir: 'docs'
-}
+  plugins: [react()],
+  base: '/p123/', // <-- IMPORTANT
 })
+
